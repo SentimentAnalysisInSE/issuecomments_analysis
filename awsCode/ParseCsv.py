c9ad1093362c82fe3dabb95001f3e0558b147528
Chloe Hebert <chhebert@calpoly.edu>
2020-04-30 16:56:17 -0400

    precision and recall

--- awsCode/ParseCsv.py
+++ awsCode/ParseCsv.py
@@ -4,22 +4,46 @@ import csv
 
 
 def Analyze(filename):
-    mismatch = 0
-    filename = './rest30.csv'
+    mismatch = -1
+    truePositivesP = -1
+    truePositivesNeg = -1
+    truePositivesN = -1
+    falsePositivesP = -1
+    falsePositivesNeg = -1
+    falsePositivesN = -1
+
+    filename = './awsJira.csv'
 
     with open(filename, newline='', encoding="ISO-8859-1") as csvfile:
         FileText = csv.reader(csvfile, delimiter='\n')
 
         for row in FileText:
-            manualLabel = row[0].split(",")[2]
-            contents = row[0].split(",")[4]
-            if (contents.lower() != manualLabel.lower()):
+            manualLabel = row[0].split(",")[2].lower()
+            contents = row[0].split(",")[4].lower()
+            if (contents!= manualLabel):
                 mismatch += 1
+                if (contents == "neutral"):
+                    falsePositivesN += 1
+                elif (contents == "positive"):
+                    falsePositivesP += 1
+                elif (contents == "negative"):
+                    falsePositivesNeg += 1
+            elif (contents == "neutral"):
+                truePositivesN += 1
+            elif (contents == "positive"):
+                truePositivesP += 1
+            elif (contents == "negative"):
+                truePositivesNeg += 1
 
 
         csvfile.close()
-
-    print(mismatch - 1)
+    print(mismatch)
+    print(str(truePositivesP) + '\n' +
+    str(truePositivesNeg) + '\n' +
+    str(truePositivesN) + '\n' +
+    str(falsePositivesP) + '\n' +
+    str(falsePositivesNeg) + '\n' +
+    str(falsePositivesN) + '\n')
 
 
 def main():
