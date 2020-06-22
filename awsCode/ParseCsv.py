f2899cdb3d5e67140eed0683789f53ec0d0525c7
Chloe Hebert <chhebert@calpoly.edu>
2020-06-22 15:24:00 -0400

    fixed positive counter

--- awsCode/ParseCsv.py
+++ awsCode/ParseCsv.py
@@ -1,49 +1,61 @@
 import sys
 import re
 import csv
+import pandas as pd
 from sklearn import metrics
 
+mismatch = 0
+truePositivesP = 0
+truePositivesNeg = 0
+truePositivesN = 0
+falsePositivesP = 0
+falsePositivesNeg = 0
+falsePositivesN = 0
+mLabels = []
+AwsLabels = []
+
+def evalLabels(data):
+    global mismatch
+    global truePositivesP
+    global truePositivesNeg
+    global truePositivesN
+    global falsePositivesP
+    global falsePositivesNeg
+    global falsePositivesN
+    global mLabels
+    global AwsLabels
+    manualLabel = data['label'].lower()
+    contents = data['AWS Labels'].lower()
+    mLabels.append(manualLabel)
+    AwsLabels.append(contents)
+
+    if (contents != manualLabel):
+        mismatch += 1
+        if (contents == "neutral"):
+            falsePositivesN += 1
+        elif (contents == "positive"):
+            falsePositivesP += 1
+        elif (contents == "negative"):
+            falsePositivesNeg += 1
+    elif (contents == "neutral"):
+        truePositivesN += 1
+    elif (contents == "positive"):
+        truePositivesP += 1
+    elif (contents == "negative"):
+        truePositivesNeg += 1
 
 def Analyze(filename):
-    mismatch = -1
-    truePositivesP = -1
-    truePositivesNeg = -1
-    truePositivesN = -1
-    falsePositivesP = -1
-    falsePositivesNeg = -1
-    falsePositivesN = -1
-
-    filename = './awsJira.csv'
+    filename = '../comment_Analysis/stackAWS.csv'
 
     with open(filename, newline='', encoding="ISO-8859-1") as csvfile:
         FileText = csv.reader(csvfile, delimiter='\n')
-        mLabels = []
-        AwsLabels = []
+        df = pd.read_csv(filename, encoding="ISO-8859-1", dtype='str')
+        df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+        df = df[['label', 'AWS Labels']]    #change to zero
 
-        for row in FileText:
-            manualLabel = row[0].split(",")[2].lower()
-            contents = row[0].split(",")[4].lower()
-            mLabels.append(manualLabel)
-            AwsLabels.append(contents)
-
-
-            if (contents!= manualLabel):
-                mismatch += 1
-                if (contents == "neutral"):
-                    falsePositivesN += 1
-                elif (contents == "positive"):
-                    falsePositivesP += 1
-                elif (contents == "negative"):
-                    falsePositivesNeg += 1
-            elif (contents == "neutral"):
-                truePositivesN += 1
-            elif (contents == "positive"):
-                truePositivesP += 1
-            elif (contents == "negative"):
-                truePositivesNeg += 1
-
-
+        df.apply(evalLabels, axis=1)
         csvfile.close()
+
     print(mismatch)
     print(str(truePositivesP) + '\n' +
     str(truePositivesNeg) + '\n' +
